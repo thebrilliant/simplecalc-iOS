7ba33f15a7bd63199c2cefe317af2bfc88b1f89e
thebrilliant <vivyanw@uw.edu>
2015-10-22 18:27:41 -0700

    fixed syntax errors and took out commented out code

--- Test-iOS/Test-iOS/ViewController.swift
+++ Test-iOS/Test-iOS/ViewController.swift
@@ -14,13 +14,13 @@ class ViewController: UIViewController {
     var clickedAction: String = ""
     var total: Double = 0.0
     var isFactorial = false
-    //var output = ""
     
     var numbers: [Double] = []
     var actions: [String] = []
     
     @IBOutlet weak var resultLabel: UILabel!
     
+    // resets all values to original state
     @IBAction func reset(sender: UIButton) {
         num = ""
         actionPressed = false
@@ -48,7 +48,6 @@ class ViewController: UIViewController {
     func divide(currentSum:Double!, newVal:Double!) -> Double {
         if newVal == 0 {
             resultLabel!.text = "SYNTAX ERROR"
-            return currentSum
         }
         return (currentSum / newVal)
     }
@@ -62,7 +61,6 @@ class ViewController: UIViewController {
     func mod(currentSum:Double!, newVal:Double!) -> Double {
         if newVal == 0 {
             resultLabel!.text = "SYNTAX ERROR"
-            return currentSum
         }
         return currentSum % newVal
     }
@@ -85,9 +83,9 @@ class ViewController: UIViewController {
                 actionPressed = false
             }
             num! = num! + text
-            //numbers.append(Double(num!)!)
             resultLabel!.text = num
         } else if resultLabel != nil && isFactorial {
+            reset(sender)
             resultLabel!.text = "SYNTAX ERROR"
         }
     }
@@ -104,8 +102,8 @@ class ViewController: UIViewController {
             isFactorial = true
             actions.append(action)
             if num!.containsString(".") {
+                reset(sender)
                 resultLabel!.text = "SYNTAX ERROR"
-                numbers.removeLast()
             }
         default:
             actions.append(action)
@@ -117,123 +115,76 @@ class ViewController: UIViewController {
         actionPressed = false
         numbers.append(Double(num!)!)
         if numbers.count <= actions.count {
+            reset(sender)
             resultLabel!.text = "SYNTAX ERROR"
-        }
-        var regular = false
-        var seen = false
-        for op in actions {
-            var current = op
-            switch current {
-            case "avg", "count", "fact (!)":
-                seen = true
-            default:
-                if seen {
-                    regular = false
-                } else {
-                    regular = true
+        } else {
+            var regular = false
+            var seen = false
+            for op in actions {
+                var current = op
+                switch current {
+                case "avg", "count", "fact (!)":
+                    seen = true
+                default:
+                    if seen {
+                        regular = false
+                    } else {
+                        regular = true
+                    }
                 }
             }
-        }
-        //var text: String = ""
-        //var count = 0
-        if regular {
-            numbers.removeFirst()
-            for numb in numbers {
-                var curAction = actions[0]
-                switch curAction {
-                case "+":
-                    total = add(total, newVal: numb)
-                case "-":
-                    total = subtract(total, newVal: numb)
-                case "x":
-                    total = multiply(total, newVal: numb)
-                case "/":
-                    total = divide(total, newVal: numb)
-                case "%":
-                    total = mod(total, newVal: numb)
+            
+            if regular {
+                numbers.removeFirst()
+                for numb in numbers {
+                    var curAction = actions[0]
+                    switch curAction {
+                    case "+":
+                        total = add(total, newVal: numb)
+                    case "-":
+                        total = subtract(total, newVal: numb)
+                    case "x":
+                        total = multiply(total, newVal: numb)
+                    case "/":
+                        total = divide(total, newVal: numb)
+                    case "%":
+                        total = mod(total, newVal: numb)
+                    default:
+                        reset(sender)
+                        resultLabel!.text = "SYNTAX ERROR"
+                    }
+                    actions.removeFirst()
+                    
+                }
+            } else {
+                var operation = actions[0]
+                switch operation {
+                case "avg":
+                    for number in numbers {
+                        total = add(total, newVal: number)
+                    }
+                    let count:String = "\(numbers.count).0"
+                    let temp = Double(count)!
+                    total = divide(total, newVal: temp)
+                case "count":
+                    let count:String = "\(numbers.count).0"
+                    total = Double(count)!
+                case "fact (!)":
+                    var number:String = "\(numbers[0])"
+                    number = number.substringToIndex(number.rangeOfString(".")!.startIndex)
+                    let intNum = Int(number)!
+                    var doubleVer = 1.0
+                    total = 1.0
+                    for i in 1...intNum {
+                        total = total * (doubleVer)
+                        doubleVer += 1.0
+                    }
                 default:
                     resultLabel!.text = "SYNTAX ERROR"
                 }
-                actions.removeFirst()
-                //text += "\(numbers.count) + "
-                
             }
-        } else {
-            var operation = actions[0]
-            switch operation {
-            case "avg":
-                //resultLabel!.text = operation
-                for number in numbers {
-                    total = add(total, newVal: number)
-                }
-                let count:String = "\(numbers.count).0"
-                let temp = Double(count)!
-                total = divide(total, newVal: temp)
-            case "count":
-                let count:String = "\(numbers.count).0"
-                total = Double(count)!
-            case "fact (!)":
-                var number:String = "\(numbers[0])"
-                number = number.substringToIndex(number.rangeOfString(".")!.startIndex)
-                let intNum = Int(number)!
-                var doubleVer = 1.0
-                total = 1.0
-                for i in 1...intNum {
-                    total = total * (doubleVer)
-                    doubleVer += 1.0
-                    //multiply(total, newVal: (i + 0.0))
-                }
-            default:
-                resultLabel!.text = "SYNTAX ERROR"
-            }
+            
+            resultLabel!.text = "\(total)"
         }
-        
-        resultLabel!.text = "\(total)"
     }
 }
-
-
-/*num! = num! + text
-resultLabel!.text = num
-//NSLog("num = \(num!)")
-} else if resultLabel != nil && actionPressed {
-num! = text
-resultLabel!.text = num*/
-
-/*if actionPressed {
-actionPressed = false
-switch clickedAction {
-case "+", "-", "/", "x", "count", "avg", "fact (!)", "%":
-switch clickedAction {
-default:
-resultLabel!.text = "SYNTAX ERROR"
-}
-default:
-resultLabel!.text = "SYNTAX ERROR"
-}
-} else {
-actionPressed = true
-switch action {
-case "+", "-", "/", "x", "count", "avg", "fact (!)", "%":
-if clickedAction == "" {
-total = add(total, newVal: Double(num!)!)
-} else {
-switch action {
-case "+":
-total = add(total, newVal: Double(num!)!)
-case "-":
-total = subtract(total, newVal: Double(num!)!)
-case "/":
-total = divide(total, newVal: Double(num!)!)
-case "x":
-total = multiply(total, newVal: Double(num!)!)
-default:
-resultLabel!.text = "SYNTAX ERROR"
-}
-}
-clickedAction = action
-resultLabel!.text = "\(total)"
-default:
-resultLabel!.text = "SYNTAX ERROR"
-}
-}*/
